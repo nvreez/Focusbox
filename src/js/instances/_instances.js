5dd4a524e7dbb5156d160be8b88419c7d0190671
nvreez <nvreez@gmail.com>
2014-10-24 20:56:00 +0200

    Added instances back in

---
+++ src/js/instances/_instances.js
@@ -0,0 +1,18 @@
+/**
+ * Add instances to the define and as function arguments, and they will automatically be accessible.
+ */
+define([
+	'instances/basic/basic'
+], function(
+	instanceBasic
+) {
+	var arg = arguments,
+		instances = {};
+
+	return function() {
+		for (var i = arg.length - 1; i >= 0; i--) {
+			instances[arg[i].name] = arg[i];
+		}
+		return instances;
+	};
+});
